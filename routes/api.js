eb241c7fef222aa8a6d00bf1fd17ee11b3812a61
omegga <whymagnet@gmail.com>
2019-04-28 03:16:50 +0200

    implement reply creation and report

--- routes/api.js
+++ routes/api.js
@@ -23,10 +23,6 @@ module.exports = function (app, db) {
         return res.send('error');
       }
       try {
-        const existingBoard = await db.collection('threads').findOne({ board, text });
-        if (existingBoard) {
-          return res.send('error');
-        }
         const date = Date.now();
         const hashedPassword = await bcrypt.hash(delete_password, SALT_ROUNDS);
         const result = await db.collection('threads').insertOne({
@@ -90,9 +86,36 @@ module.exports = function (app, db) {
       }
       try {
         const threadsList = await db.collection('threads')
-          .find({ board })
-          .project({ delete_password: 0, reported: 0 })
-          .sort({ bumped_on: -1 })
+          .aggregate([
+            {
+              $match: { board }
+            },
+            {
+              $project: { delete_password: 0, reported: 0 }
+            },
+            {
+              $unwind: "$replies"
+            },
+            {
+              $sort: { "replies.created_on": -1 }
+            },
+            {
+              $group: {
+                _id: "$_id",
+                text: { $first: "$text" },
+                board: { $first: "$board" },
+                created_on: { $first: "$created_on" },
+                bumped_on: { $first: "$bumped_on" },
+                replies: { $push: "$replies" }
+              }
+            },
+            {
+              $addFields: {
+                "replies": { $slice: [ "$replies", 3 ] },
+                "replycount": { $size: "$replies" }
+              }
+            }
+          ])
           .toArray();
         return res.json(threadsList);
       } catch (e) {
@@ -101,6 +124,62 @@ module.exports = function (app, db) {
       }
     });
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .post(async (req, res) => {
+      const { text, delete_password, thread_id } = req.body;
+      const board = req.body.board || req.params.board;
+      if (!board || !text || !delete_password || !thread_id) {
+        return res.send('error');
+      }
+      try {
+        const hashedPassword = await bcrypt.hash(delete_password, SALT_ROUNDS);
+        const date = Date.now();
+        const result = await db.collection('threads')
+          .findOneAndUpdate(
+            { _id: ObjectID(thread_id) }, 
+            { 
+              $push: { 
+                replies: {
+                  _id: new ObjectID(),
+                  text,
+                  created_on: new Date(date),
+                  delete_password: hashedPassword,
+                  reported: false
+                }
+              },
+              $set: {
+                bumped_on: new Date(date)
+              }
+            }
+          );
+        if (!result.ok || result.value === null) {
+          return res.send('error');
+        }
+        return res.redirect(`/b/${board}/${thread_id}`);
+      } catch (e) {
+        console.log(e);
+        return res.send('error');
+      }
+    })
+    .put(async (req, res) => {
+      const { thread_id, reply_id } = req.body;
+      if (!thread_id || !reply_id) {
+        return res.send('error');
+      }
+      try {
+        const modifyResult = await db.collection('threads')
+          .findOneAndUpdate(
+            { _id: new ObjectID(thread_id), "replies._id": new ObjectID(reply_id) }, 
+            { $set: { "replies.$.reported": true } }
+          );
+        if (!modifyResult.ok || modifyResult.value === null) {
+          return res.send('error');
+        }
+        return res.send('success');
+      } catch (e) {
+        console.error(e);
+        return res.send('error');
+      }
+    });
 
 };
